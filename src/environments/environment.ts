4395ba4169bd9d7ba0e39f15ba7e465193ab6674
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-09-21 10:37:47 +0200

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false
+  firebaseConfig: {
+    apiKey: "AIzaSyAWKpDlydqMRrjB3BcSJPJ6bg_qjkgascE",
+    authDomain: "angular-oauth-35e12.firebaseapp.com",
+    projectId: "angular-oauth-35e12",
+    storageBucket: "angular-oauth-35e12.appspot.com",
+    messagingSenderId: "564612093101",
+    appId: "1:564612093101:web:bfb47bd35ada549ca2a224"
+  }
+};
